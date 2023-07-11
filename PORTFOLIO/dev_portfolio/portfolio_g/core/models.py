a3e641d614fb5a1fd45f9c512bf8b3f9b9202c8b
RankracerBR <101043200+RankracerBR@users.noreply.github.com>
2023-07-11 02:47:18 -0300

    update

--- PORTFOLIO/dev_portfolio/portfolio_g/core/models.py
+++ PORTFOLIO/dev_portfolio/portfolio_g/core/models.py
@@ -2,6 +2,17 @@ from django.db import models
 
 # Create your models here.
 
-class Email:
+class Email(models.Model):
     def __init__(self, nome, email,comentario):
-        pass
+        nome = models.CharField(max_length=100, default="Nome")
+        email = models.CharField(max_length=100, default="Email")
+        comentario = models.CharField(max_length=900, default="Email")
+        
+        def __str__(self):
+            return self.nome
+        
+        def __str__(self):
+            return self.email
+        
+        def __str__(self):
+            return self.comentario

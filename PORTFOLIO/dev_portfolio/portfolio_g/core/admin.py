28a946f5431ead1b8858c62470f8c96965603a22
RankracerBR <101043200+RankracerBR@users.noreply.github.com>
2023-07-11 14:10:32 -0300

    update

--- PORTFOLIO/dev_portfolio/portfolio_g/core/admin.py
+++ PORTFOLIO/dev_portfolio/portfolio_g/core/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Usuarios
 
 # Register your models here.
+
+class UsuariosAdmin(admin.ModelAdmin):
+    list_display = ('nome','comentario')
+    list_filter = ('nome',)
+
+admin.site.register(Usuarios,UsuariosAdmin)
--- PORTFOLIO/dev_portfolio/portfolio_g/core/models.py
+++ PORTFOLIO/dev_portfolio/portfolio_g/core/models.py
@@ -2,17 +2,16 @@ from django.db import models
 
 # Create your models here.
 
-class Email(models.Model):
-    def __init__(self, nome, email,comentario):
-        nome = models.CharField(max_length=100, default="Nome")
-        email = models.CharField(max_length=100, default="Email")
-        comentario = models.CharField(max_length=900, default="Email")
-        
-        def __str__(self):
-            return self.nome
-        
-        def __str__(self):
-            return self.email
-        
-        def __str__(self):
-            return self.comentario
+class Usuarios(models.Model):
+    nome = models.CharField(max_length=100, default='Nome')
+    comentario = models.CharField(max_length=500, default="Comentario")
+    
+    class Meta:
+        db_table = 'usuarios'
+        verbose_name_plural = 'usuarios'
+    
+    def __str__(self):
+        return self.nome
+    
+    def __str__(self):
+        return self.comentario
